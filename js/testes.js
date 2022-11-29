5b173bd29a042de2de49bb8db221f9cdc9e43052
Breno-S <114250412+Breno-S@users.noreply.github.com>
2022-11-28 22:47:04 -0300

    Add files via upload

---
+++ js/testes.js
@@ -0,0 +1,7 @@
+
+let topo = document.getElementById("topo");
+
+function topFunction() {
+    document.body.scrollTop = 0; // For Safari
+    document.documentElement.scrollTop = 0; // For Chrome, Firefox, IE and Opera
+}
